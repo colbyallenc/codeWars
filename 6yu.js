1d3f9d2ea2cac9b6c84b266d1c70d2103f15d22d
colbyallenc <colbyallenc@gmail.com>
2018-10-17 08:29:53 -0400

    6yu

--- 6yu.js
+++ 6yu.js
@@ -99,3 +99,14 @@ function array_diff(a, b) {
      } 
   return newArr
  }
+
+//  ____________________________________________________
+//  Write a reverseWords function that accepts a string a parameter, and reverses each word in the string. Any spaces in the string should be retained.
+
+// Example:
+
+reverseWords("This is an example!"); // returns "sihT si na !elpmaxe" reverseWords("double spaces"); // returns "elbuod secaps"
+
+function reverseWords(str) {
+  return  str.split('').reverse().join('').split(' ').reverse().join(' ')
+}

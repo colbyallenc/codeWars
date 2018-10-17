193ad92dcd27984e54760ac298b5bdad63901901
colbyallenc <colbyallenc@gmail.com>
2018-10-17 08:29:32 -0400

    5yu

--- 5yu.js
+++ 5yu.js
@@ -168,3 +168,35 @@ function twoSums(numbers, target){
  
  twoSums([2,4,7,4, 8,5,3,1], 9)
 
+// ____________________________________________
+
+/* Given an array and a value, remove all instances of that value in-place and return the new length.
+
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+
+Example:
+
+Given nums = [3,2,2,3], val = 3,
+
+Your function should return length = 2, with the first two elements of nums being 2. */
+
+const removeElement = (arr, target) => {
+  let length = arr.length;
+  for (var i = 0; i < length; i++) {
+    // if the value at current index matches the target value
+    if (arr[i] === target) {
+      // remove current index
+      arr.splice(i, 1);
+      // subtract 1 from length
+      length--;
+    }
+  }
+  return arr.length;
+}
+
+console.log(removeElement([3,2,2,3], 3));
+// console.log(removeElement([1,2,2,4], 3));
+// console.log(removeElement([1,2,2,4], 3));
+// console.log(removeElement([1,2,3,4,5,6], 3));

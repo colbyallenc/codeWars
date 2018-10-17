071b20202968b1c56d4594b7fa1be04534833cb5
colbyallenc <colbyallenc@gmail.com>
2018-10-17 10:16:22 -0400

    5yu

--- 5yu.js
+++ 5yu.js
@@ -200,3 +200,24 @@ console.log(removeElement([3,2,2,3], 3));
 // console.log(removeElement([1,2,2,4], 3));
 // console.log(removeElement([1,2,2,4], 3));
 // console.log(removeElement([1,2,3,4,5,6], 3));
+
+
+// ________________________________________________________
+
+// You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security system connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+// Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.
+
+function rob(input){
+  let store = 0
+  // i want to traverse through the array 
+for(var i=0; i < input.length; i++){
+  // if the [i] is odd i want to select that input and add it to the saved store
+  if(i % 2===0){
+    store += input[i] 
+  }
+}
+  return store
+}
+
+rob([1,2,3,4,5])

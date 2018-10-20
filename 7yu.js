ab9fc24ea05ca62e2ea456520d17b4e260ac70e1
colbyallenc <colbyallenc@gmail.com>
2018-10-20 17:58:32 -0400

    breaking chocolate solution

--- 7yu.js
+++ 7yu.js
@@ -47,3 +47,38 @@ function isPangram(string){
   
   isPangram("The quick brown fox jumps over the lazy dog.")
   // isPangram("The quick jumps over the lazy dog.")
+  // _________________________________________
+
+  // Breaking Chocolate Problem
+
+// Your task is to split the chocolate bar of given dimension n x m into small squares. Each square is of size 1x1 and unbreakable. Implement a function that will return minimum number of breaks needed.
+
+// For example if you are given a chocolate bar of size 2 x 1 you can split it to single squares in just one break, but for size 3 x 1 you must do two breaks.
+
+// If input data is invalid you should return 0 (as in no breaks are needed if we do not have any chocolate to split). Input will always be a non-negative integer.
+
+// here you can see that the result would equal (n - 1)(m[i])
+// n=position m=index
+// 1x1 = 0  2x1 = 1   3x1 = 2
+// 1x2 = 1  2x2 = 2   3x2 = 3
+// 1x3 = 2  2x3 = 3   3x3 = 4
+// 1x4 = 3  2x4 = 4   3x4 = 5
+// 1x5 = 4  2x5 = 5   3x5 = 6
+
+// 2x6 :
+// (n-1)+(m-1) = (2-1)+(6-1) = 6
+                // 1  +  5  = 6 breaks
+
+function chocolate(n,m){
+  let result = (n-1)+(m-1);
+  if(result < 0 || undefined){
+    return 0
+  }
+  return result
+}
+
+// chocolate(2,2);
+chocolate(0,1);
+// chocolate(2,2);
+// chocolate(1,4);
+// chocolate(2,5);
--- 8yu.js
+++ 8yu.js
@@ -167,3 +167,4 @@ function oddCount(n){
 
 
 
+

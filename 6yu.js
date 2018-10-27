21a675cea59d3178cf5806cf5019e9c593c75ef9
colbyallenc <colbyallenc@gmail.com>
2018-10-27 14:01:45 -0400

    6yu solutions

--- 6yu.js
+++ 6yu.js
@@ -164,3 +164,92 @@ var climbStairs = function(n) {
 
     return W[n];
 };
+// _______________________________________________________
+
+
+// You get an array of arrays.
+// If you sort the arrays by their length, you will see, that their length-values are consecutive.
+// But one array is missing!
+
+
+// You have to write a method, that return the length of the missing array.
+
+// Example:
+// [[1, 2], [4, 5, 1, 1], [1], [5, 6, 7, 8, 9]] --> 3
+
+
+// If the array of arrays is null/nil or empty, the method should return 0.
+
+// When an array in the array is null or empty, the method should return 0 too!
+// There will always be a missing element and its length will be always between the given arrays. 
+
+// Have fun coding it and please don't forget to vote and rank this kata! :-)
+
+// I have created other katas. Have a look if you like coding and challenges.
+
+function getLengthOfMissingArray(arrayOfArrays) {
+  let sortedArray = []
+  let missingNum = []
+ 
+  if(arrayOfArrays === null)
+  return 0
+
+    for(let i = 0; i < arrayOfArrays.length; i++) {
+        if(arrayOfArrays[i] === null || arrayOfArrays[i].length === 0) 
+            return 0;
+    }
+
+  arrayOfArrays.map(array=>{
+    sortedArray.push(array.length)
+    sortedArray.sort()
+  })
+
+  sortedArray.forEach(num=> {
+    if(!sortedArray.includes(num-1)){
+      // console.log(num+1)
+      missingNum.push(num-1)
+      } 
+  })
+  return missingNum
+}
+
+getLengthOfMissingArray([[1, 2], [4, 5, 1, 1], [1], [5, 6, 7, 8, 9]])
+
+
+/* You get an array of arrays.
+If you sort the arrays by their length, you will see, that their length-values are consecutive.
+But one array is missing!
+
+You have to write a method, that return the length of the missing array.
+
+Example:
+[[1, 2], [4, 5, 1, 1], [1], [5, 6, 7, 8, 9]] --> 3
+
+
+If the array of arrays is null/nil or empty, the method should return 0.
+
+When an array in the array is null or empty, the method should return 0 too!
+There will always be a missing element and its length will be always between the given arrays
+*/
+
+
+function getLengthOfMissingArray(arrOfArr) {
+  // If the array of arrays is null/nil or empty, the method should return 0
+  if(arrOfArr === null)
+  return 0;
+  
+  for(let i = 0; i < arrOfArr.length; i++) {
+      // When an array in the array is null or empty, the method should return 0 too!
+      if(arrOfArr[i] === null || arrOfArr[i].length === 0) 
+          return 0;
+  }
+  
+  arrOfArr = arrOfArr.sort(function(a,b) {return a.length > b.length ? 1 : -1});
+  for(let i = 1; i < arrOfArr.length; i++) {
+      if(arrOfArr[i].length != arrOfArr[i-1].length + 1) 
+          return arrOfArr[i-1].length + 1;
+  }
+  return 0;
+}
+
+console.log(getLengthOfMissingArray([[1, 2], [4, 5, 1, 1], [1], [5, 6, 7, 8, 9]])) // => 3
